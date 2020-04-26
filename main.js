17b83a8e0617035f55f52ad373419958b8d72b13
tina-fernan <tinaaroom@gmail.com>
2020-04-26 16:01:38 +0200

    update  commit

--- main.js
+++ main.js
@@ -5,3 +5,20 @@ function changeText()
   quoteBtn.style.backgroundColor="grey";
 }
 quoteBtn.addEventListener("click",changeText);
+
+let loginPwdStatus=false;
+function changePwdView(){
+  let getLoginInput= document.getElementById("loginPwdChange");
+  if(loginPwdStatus==false)
+  {
+    getLoginInput.setAttribute("type","text");
+    loginPwdStatus=true;
+  }
+  else if(loginPwdStatus==true)
+  {
+    getLoginInput.setAttribute("type","password");
+    loginPwdStatus=false;
+
+  }
+
+}
